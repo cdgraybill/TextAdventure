40d7afe3a9a0746a1bc77d9a6fd04a025430717f
Cameron Graybill <cdgraybill@gmail.com>
2020-01-24 14:11:43 -0600

    Add project files.

---
+++ TextAdventure/PlayerCharacter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextAdventure
+{
+    class PlayerCharacter
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+    }
+}
---
+++ TextAdventure/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TextAdventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Text Adventure! What's your characters name?");
+            PlayerCharacter player = new PlayerCharacter();
+            player.Name = Console.ReadLine();
+
+
+            Console.WriteLine($"Hey, {player.Name}! Press any key to start!");
+            Console.ReadLine();
+
+
+        }
+    }
+}
